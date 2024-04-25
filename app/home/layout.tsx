b3cc3e1d1af7dd5d284585975b4361063ffd55ac
Paul Ebert <paul.ebert@code.berlin>
2024-04-25 10:58:00 +0200

    remove parallel routing in hiragana section

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,14 +1,3 @@
-export default function Layout({
-  children,
-  hiragana,
-}: {
-  children: React.ReactNode;
-  hiragana: React.ReactNode;
-}) {
-  return (
-    <div>
-      {children}
-      {hiragana}
-    </div>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
 }
