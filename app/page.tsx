9f122a0fd8a599b3f360b62c7386b00a05b4b786
Paul Ebert <paul.ebert@code.berlin>
2024-04-25 18:27:04 +0200

    style landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import isAuthenticated from "@/middleware/isAuthenticated";
+import { ArrowRight } from "@carbon/icons-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -9,9 +10,25 @@ export default function Home() {
   console.log(isAuthenticated());
 
   return (
-    <div>
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
+    <div className="grid h-screen place-content-center gap-16">
+      <div>
+        <p className="heading-04">Kana</p>
+        <p className="heading-03">Manabu</p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Link href="/login">
+          <div className="buttonSquare">
+            <p>Log in to your Account</p>
+            <ArrowRight />
+          </div>
+        </Link>
+        <Link href="/register">
+          <div className="buttonSquare">
+            <p>Create an account</p>
+            <ArrowRight />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
