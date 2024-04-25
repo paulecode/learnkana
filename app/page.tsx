3ba707b161cfafab4bceefa0d45b5d07275d11d5
Paul Ebert <paul.ebert@code.berlin>
2024-04-25 17:23:10 +0200

    adjust homepage with autoredirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import isAuthenticated from "@/middleware/isAuthenticated";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  if (isAuthenticated()) {
+    redirect("/home");
+  }
+  console.log(isAuthenticated());
+
   return (
     <div>
-      <Link href="/home">Homepage</Link>
       <Link href="/login">Login</Link>
       <Link href="/register">Register</Link>
     </div>
