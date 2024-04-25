c4ee17ad3dffcc415fd24d7e4ebbbb173123059f
Paul Ebert <paul.ebert@code.berlin>
2024-04-25 15:13:47 +0200

    rename route

--- app/api/getQuizResults/route.ts
+++ app/api/getQuizResults/route.ts
@@ -13,8 +13,16 @@ export async function GET(req: NextRequest) {
 
   const answers = await prisma.kanaQuizChallenge.findMany({
     where: { kanaQuizSessionId: quizSession.id },
-    select: { challenge: true, answer: true, givenAnswer: true },
+    select: { challenge: true, answer: true, givenAnswer: true, id: true },
   });
 
-  return NextResponse.json({ success: true, answers }, { status: 200 });
+  const totalAnswers = answers.length;
+  const correctAnswers = answers.filter(
+    (answer) => answer.answer == answer.givenAnswer,
+  ).length;
+
+  return NextResponse.json(
+    { success: true, answers, totalAnswers, correctAnswers },
+    { status: 200 },
+  );
 }
