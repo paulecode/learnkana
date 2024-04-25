a74750d9ccbf490d36e0ac1141bebd5cadbbf199
Paul Ebert <paul.ebert@code.berlin>
2024-04-25 15:57:33 +0200

    add toast component

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,3 +1,10 @@
+import { Toaster } from "@/components/ui/toaster";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      {children}
+      <Toaster />
+    </div>
+  );
 }
