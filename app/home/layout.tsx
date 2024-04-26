e5c50080ce6befbee0de8726e1eb7be03716ef4d
Paul E <78560094+paulecode@users.noreply.github.com>
2024-04-26 17:21:28 +0200

    Update layout.tsx
    h-dvh

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,7 +3,7 @@ import UserDropDownWrapper from "@/compounds/UserDropDown/UserDropDownWrapper";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-dvh w-full flex-col">
       <div className="border-b border-gray-200 shadow">
         <UserDropDownWrapper />
       </div>
