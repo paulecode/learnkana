da442f3372df6d3883818d840095248e326cc637
Paul E <78560094+paulecode@users.noreply.github.com>
2024-04-26 18:20:21 +0200

    Update ResultTable.tsx
    Edit string

--- compounds/ResultTable/ResultTable.tsx
+++ compounds/ResultTable/ResultTable.tsx
@@ -8,7 +8,7 @@ const ResultTable: React.FC<{ results: SelectedKanaQuizChallenge[] }> = ({
     <div className="mb-16 grid grid-cols-[1fr_1fr_1fr_min-content] gap-4">
       <div className="col-span-full grid grid-cols-subgrid justify-items-center">
         <p className="heading-01">Question</p>
-        <p className="heading-01">Correct Answer</p>
+        <p className="heading-01">Solution</p>
         <p className="heading-01">Your Answer</p>
       </div>
       <div className="col-span-full grid grid-cols-subgrid">
